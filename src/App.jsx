1866ced5b8a252f0077f58e6a2373681e9ee519d
Misbah Sehar <misbahsahar09@gmail.com>
2026-01-13 11:55:43 +0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
+      <div className="bg-white p-8 rounded-2xl shadow-xl text-center w-96">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Tailwind is Working 🚀
+        </h1>
+
+        <p className="text-gray-500 mb-6">
+          Your React + Vite + Tailwind setup is complete.
+        </p>
+
+        <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-lg transition">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
